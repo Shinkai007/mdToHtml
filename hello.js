41c52ca7c00c2521d535ef33f344fec1813d3eb4
Shinkai007 <499565606@qq.com>
2023-05-14 12:55:18 +0100

    初始化了项目库

---
+++ hello.js
@@ -0,0 +1,8 @@
+var str = 'hello world';
+console.log('str', str);
+console.log("str ".concat(str));
+function greet(person) {
+    return 'Hello, ' + person;
+}
+console.log(greet("TypeScript"));
+//# sourceMappingURL=hello.js.map
---
+++ hello.ts
@@ -0,0 +1,9 @@
+let str: string = 'hello world'
+console.log('str',str)
+console.log(`str ${str}`)
+
+function greet(person: string) {
+    return 'Hello, ' + person;
+}
+
+console.log(greet("TypeScript"));
